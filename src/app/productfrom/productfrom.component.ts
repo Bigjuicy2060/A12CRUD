6598fb2f0df7c9d51fae6cbc5d2cf7181bbbbd5b
Bigjuicy2060 <amjadyounis7@gmail.com>
2022-12-27 17:13:39 +0200

    initial commit components

---
+++ src/app/productfrom/productfrom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productfrom',
+  templateUrl: './productfrom.component.html',
+  styleUrls: ['./productfrom.component.css']
+})
+export class ProductfromComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
